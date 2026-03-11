7e619b2f044e04ad5eadacab22a7ca926ecee821
augustoburatto <54562178+augustoburatto@users.noreply.github.com>
2026-03-11 14:28:33 -0300

    Change token

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const ghOwner = document.getElementById('gh-owner');
 const ghRepo = document.getElementById('gh-repo');
 const ghBranch = document.getElementById('gh-branch');
 const ghPath = document.getElementById('gh-path');
-const ghToken = "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";
+const ghToken = "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";
 
 let itens = [];
 
@@ -207,7 +207,7 @@ async function carregarDoGithub() {
 async function salvarNoGithub() {
   salvarConfigGithub();
   const url = githubApiUrl();
-  const token = "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";
+  const token = "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";
 
   if (!url) {
     statusSalvar('Preencha owner e repositório para salvar no GitHub.', true);
